df7a8d3795fc5cb35ccc9cadf1ff09afa408e247
Alexander Svendsen <asv009@post.uit.no>
2014-03-09 19:17:27 +0100

    Added a config parser to the project, so the password is not saved in the actual code and not commited to git

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 
 TWITCH_SERVER_HOST = 'irc.twitch.tv'
 TWITCH_SERVER_PORT = 6667
+CHANNEL = '#vinzet'
+
+CONFIG_FILE = "personal_info.ini"
--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 from irc_client import *
+from config import *
+import ConfigParser
+import os.path
 
-#sets variables for connection to twitch chat
-nick = 'segment_fault'
-channel = '#vinzet'
-password = 'XXXX'
+Config = ConfigParser.ConfigParser()
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                print ("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
 
+nick = ""
+password = ""
+
+if os.path.isfile(CONFIG_FILE):
+    print "Found config, reading nick and password"
+    Config.read(CONFIG_FILE)
+    nick = ConfigSectionMap("personal_info")['nick']
+    password = ConfigSectionMap("personal_info")['password']
+else:
+    print "No config found, Input nick and password, Hit enter to continue"
+    nick = raw_input("NICK: ")
+    password = raw_input("PASSWORD: ")
+
+    cfgfile = open(CONFIG_FILE, 'w')
+    Config.add_section('personal_info')
+    Config.set('personal_info', 'nick', nick)
+    Config.set('personal_info', 'password', password)
+    Config.write(cfgfile)
+    cfgfile.close()
+
+
+print "Connecting to twitch"
 
 irc = TwitchIRCClient(nick, password)
 irc.start()
@@ -15,7 +49,7 @@ while not irc.connected:
 
 print "Connected!"
 
-irc.join_channel(channel)
+irc.join_channel(CHANNEL)
 
 while True:
     time.sleep(0.1)
